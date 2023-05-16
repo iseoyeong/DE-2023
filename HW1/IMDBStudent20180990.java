285e669e14784c7bb229f22619ed82e3eb2a0903
iseoyeong <zjarhk21@naver.com>
2023-05-16 14:03:45 +0900

    upload IMDBS java code

--- HW1/IMDBStudent20180990.java
+++ HW1/IMDBStudent20180990.java
@@ -9,10 +9,67 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.fs.FSDataInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 
 public class IMDBStudent20180990 {
 
+	public static class GenreCountMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+    		private final static IntWritable one = new IntWritable(1);
+    		private Text word = new Text();
+
+    		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String genre = value.toString().split("::")[2];
+
+			//System.out.println(genre);
+        		String[] genres = genre.split("\\|");
+
+        		for (String g : genres) {
+				//System.out.println(g);
+            			word.set(g);
+            			context.write(word, one);
+        		}
+    		}
+	}
+
+
+	public static class GenreCountReducer extends Reducer<Text,IntWritable,Text,IntWritable>
+    	{
+            	private IntWritable result = new IntWritable();
+
+            	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+            	{
+                    	int sum = 0;
+                    	for (IntWritable val : values)
+                    	{
+                            	sum += val.get();
+                    	}
+                    	result.set(sum);
+                    	context.write(key, result);
+            	}
+    	}
+
+    	public static void main(String[] args) throws Exception
+    	{
+            	Configuration conf = new Configuration();
+            	String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+            	if (otherArgs.length != 2)
+            	{
+                    	System.err.println("Usage: genrecount <in> <out>");
+                    	System.exit(2);
+            	}
+            	Job job = new Job(conf, "genre count");
+            	job.setJarByClass(IMDBStudent20180990.class);
+            	job.setMapperClass(GenreCountMapper.class);
+            	job.setCombinerClass(GenreCountReducer.class);
+            	job.setReducerClass(GenreCountReducer.class);
+            	job.setOutputKeyClass(Text.class);
+            	job.setOutputValueClass(IntWritable.class);
+
+            	FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+            	FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+            	FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+            	System.exit(job.waitForCompletion(true) ? 0 : 1);
+    	}
+}
 
